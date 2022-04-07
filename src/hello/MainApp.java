e248fe6d7afffb36c72c058186559b1121d0981e
Barlow-Liu <1299982133@qq.com>
2022-04-07 14:14:17 +0800

    属性注入

--- src/hello/MainApp.java
+++ src/hello/MainApp.java
@@ -1,19 +1,23 @@
 package hello;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+
 public class MainApp {
+	private static final Log LOGGER = LogFactory.getLog(MainApp.class);
+
     public static void main(String[] args) {
         @SuppressWarnings("resource")
 		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
         HelloWorld obj = context.getBean("helloWorld",HelloWorld.class);
         obj.getMessage();
 
+        Student student= context.getBean("student",Student.class);
+        LOGGER.info(student.toString());
 
-//test
-        //test2
-        //test3
-        //test4
+
     }
 }
